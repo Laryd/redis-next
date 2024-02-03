1acaf53c07f3ca4f136629bf5de4467c6889d682
Laryd <hillarymuboka@gmail.com>
2024-02-04 01:59:47 +0300

    feat: optimization remove many awaits and comments page add

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -5,25 +5,24 @@ import { redis } from "@/lib/redis";
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
-
+    
     const { text, tags } = body;
-
+    
     const commentId = nanoid();
-
-    // Add comment to list
-    await redis.rpush("comments", commentId);
-
-    // Add tags to comments
-    await redis.sadd(`tags:${commentId}`, tags);
-
     // Retrieve and store comment details
     const comment = {
       text,
       timestamp: new Date(),
       author: req.cookies.get("userId")?.value,
     };
+    
+    await Promise.all([
+        redis.rpush("comments", commentId),
+        redis.sadd(`tags:${commentId}`, tags),
+        redis.hset(`comment_details:${commentId}`, comment)
+    ])
 
-    await redis.hset(`comment_details:${commentId}`, comment);
+
 
     return new Response("OK");
   } catch (err) {
